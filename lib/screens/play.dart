5ee1b6c8b437cea3330b7640adf46de9120a4e6c
Matheus <mrljorge@outlook.com>
2022-08-07 22:12:25 -0300

    add throttle overlay

--- lib/screens/play.dart
+++ lib/screens/play.dart
@@ -12,6 +12,58 @@ class GamePlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FlyGame game = FlyGame(context, airplane);
-    return GameWidget(game: game);
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GameWidget(game: game),
+            ThrottleStick(game: game),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThrottleStick extends StatefulWidget {
+  final FlyGame game;
+  const ThrottleStick({
+    Key? key,
+    required this.game,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => ThrottleStickState();
+}
+
+class ThrottleStickState extends State<ThrottleStick> {
+  double sliderValue = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 50,
+      bottom: 50,
+      child: RotatedBox(
+        quarterTurns: -1,
+        child: Container(
+          width: 200,
+          height: 50,
+          color: Colors.grey.withAlpha(100),
+          child: Slider(
+            min: 0,
+            max: 100,
+            value: sliderValue,
+            onChanged: (value) {
+              setState(() {
+                widget.game.airplane.power = value;
+                sliderValue = value;
+              });
+              print(widget.game.airplane.power);
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
